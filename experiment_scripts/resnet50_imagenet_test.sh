8c2370aa1035dcbdab5cecda7c6642d7a68bd8a4
Guoxuan Xia <gx221@ee-sleipnir.ee.ic.ac.uk>
2021-11-22 17:09:11 +0000

    forgot to save file

--- experiment_scripts/resnet50_imagenet_test.sh
+++ experiment_scripts/resnet50_imagenet_test.sh
@@ -4,8 +4,6 @@ cd ..
 # path the configuration file
 config_path=experiment_configs/resnet50_imagenet.json
 
-echo "only testing"
-
 # rather than using a generated weights file from training
 # just use the pretrained on provided by pytorch
 python test.py $config_path --seed 1 --gpu 0 --weights_path models/saved_models/resnet50_imagenet/resnet50-19c8e357.pth
