9e2bc826b9ba0fd98eb9a1a81b867bc3c8fa126b
Guoxuan Xia <gx221@ee-sleipnir.ee.ic.ac.uk>
2021-11-23 13:04:45 +0000

    added model directories to readme

--- experiment_scripts/mobilenetv2_imagenet_test.sh
+++ experiment_scripts/mobilenetv2_imagenet_test.sh
@@ -6,5 +6,5 @@ config_path=experiment_configs/mobilenetv2_imagenet.json
 
 # rather than using a generated weights file from training
 # just use the pretrained on provided by pytorch
-python test.py $config_path --seed 1 --gpu 0 --weights_path models/saved_models/mobilnetv2_imagenet/mobilenet_v2-b0353104.pth
+python test.py $config_path --seed 1 --gpu 0 --weights_path models/saved_models/mobilenetv2_imagenet/mobilenet_v2-b0353104.pth
 python plot_results.py $config_path --num_runs 1
